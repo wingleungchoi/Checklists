2eb0628c5480f71c56d739ce2749408db9bdca77
wingleungchoi <wingleungchoi@gmail.com>
2015-06-14 18:35:34 +0800

    able to return 5 rows

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +19,12 @@ class ChecklistViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5 }
+    override func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem") as! UITableViewCell
+        return cell }
 }
 
