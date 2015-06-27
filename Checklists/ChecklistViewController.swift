49d641e29b56060f73710882c48e587d9a041954
wingleungchoi <wingleungchoi@gmail.com>
2015-06-27 16:24:10 +0800

    able click checkmark

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -21,10 +21,42 @@ class ChecklistViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5 }
+        return 100
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+         //let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem", forIndexPath: indexPath) as! UITableViewCell
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+            
+        if indexPath.row % 5 == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Soccer practise"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Eat ice cream"
+        }
+            
+        return cell
+    }
+    
     override func tableView(tableView: UITableView,
-        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem") as! UITableViewCell
-        return cell }
+        didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            if let cell = tableView.cellForRowAtIndexPath(indexPath){
+                if cell.accessoryType == .None {
+                    cell.accessoryType = .Checkmark
+                } else {
+                    cell.accessoryType = .None
+                }
+            }
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 }
 
