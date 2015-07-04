fe4835d588d9a20a31fe5925bec3606b43356d2a
wingleungchoi <wingleungchoi@gmail.com>
2015-07-04 16:59:39 +0800

    change itemviewtable to itemview

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -1,9 +1,46 @@
 //
-//  AddItemViewController.swift
+//  AddItemTableViewController.swift
 //  Checklists
 //
-//  Created by Wing LeungCHOI on 4/7/15.
+//  Created by Wing LeungCHOI on 1/7/15.
 //  Copyright (c) 2015 WingLeung CHOI. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol AddItemViewControllerDelegate: class {
+    func addItemViewControllerDidCancel(controller: AddItemViewController)
+    func addItemViewController(controller: AddItemViewController, didFinishAddingItem item: ChecklistItem)
+}
+
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    @IBAction func cancel() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func done() {
+        println("Contents of the text field: \(textField.text)")
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func tableView(tableView: UITableView,
+        willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+            return nil
+    }
+    
+    override func viewWillAppear(anitmated: Bool) {
+        super.viewWillAppear(anitmated)
+        textField.becomeFirstResponder()
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let oldText: NSString = textField.text
+        let newText: NSString = oldText.stringByReplacingCharactersInRange(range, withString: string)
+        doneBarButton.enabled = (newText.length > 0)
+        
+        return true
+    }
+}
