d82bc311327b59e7a465b2db0db19b55803dd5a6
wingleungchoi <wingleungchoi@gmail.com>
2015-07-18 18:25:05 +0800

    before add iconpickerViewController

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -56,6 +56,7 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
             } else {
                 cell.detailTextLabel!.text = "\(count) Remaining"
             }
+            cell.imageView!.image = UIImage(named: checklist.iconName)
         }
         return cell
     }
--- Checklists/Checklist.swift
+++ Checklists/Checklist.swift
@@ -11,18 +11,22 @@ import UIKit
 class Checklist: NSObject, NSCoding {
     var name = ""
     var items: [ChecklistItem] = [ChecklistItem]()
+    var iconName:String
     required init(coder aDecoder: NSCoder) {
         name = aDecoder.decodeObjectForKey("Name") as! String
         items = aDecoder.decodeObjectForKey("Items") as! [ChecklistItem]
+        iconName = aDecoder.decodeObjectForKey("IconName") as! String
         super.init()
     }
     init(name: String){
         self.name = name
+        iconName = "No Icon"
         super.init()
     }
     func encodeWithCoder(aCoder: NSCoder) {
         aCoder.encodeObject(name, forKey: "Name")
         aCoder.encodeObject(items, forKey: "Items")
+        aCoder.encodeObject(iconName, forKey: "IconName")
     }
     
     func countUncheckedItems() -> Int {
